2f28114af0162c1f5bc8a642ec9440140f5d41b7
Erick <erickungu82@gmail.com>
2023-10-08 14:52:03 +0300

    Update the makeGrid function as it made 15 divs instead of 16

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const sketchFace = document.querySelector('.sketch-face')
 const colorSelection = document.querySelector('.color-selection')
 
 function makeGrid() {
-    for (let i = 1; i < 16; i++) {
+    for (let i = 1; i <= 16; i++) {
         const gridRow = document.createElement('div');
         gridRow.className = 'grid-row';
         sketchFace.appendChild(gridRow);
     }
 
-    for (let j = 1; j < 16; j++) {
+    for (let j = 1; j <= 16; j++) {
         const gridColumn = document.createElement('div');
         gridColumn.className = 'grid-column';
         sketchFace.appendChild(gridColumn);
     }
 }
+
+makeGrid()
