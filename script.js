f6c22579683abf7d67ad99bb03e9893eeea61e89
Erick <erickungu82@gmail.com>
2023-10-08 14:20:09 +0300

    Add the for loops to a function make Grid

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const containerDiv = document.querySelector('.container')
 const sketchFace = document.querySelector('.sketch-face')
 const colorSelection = document.querySelector('.color-selection')
 
-for (let i = 1; i < 16; i++) {
-    const gridRow = document.createElement('div');
-    gridRow.className = 'grid-row';
-    sketchFace.appendChild(gridRow);
-}
+function makeGrid() {
+    for (let i = 1; i < 16; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.className = 'grid-row';
+        sketchFace.appendChild(gridRow);
+    }
 
-for (let j = 1; j < 16; j++) {
-    const gridColumn = document.createElement('div');
-    gridColumn.className = 'grid-column';
-    sketchFace.appendChild(gridColumn);
+    for (let j = 1; j < 16; j++) {
+        const gridColumn = document.createElement('div');
+        gridColumn.className = 'grid-column';
+        sketchFace.appendChild(gridColumn);
+    }
 }
