19781b6e461ab168a7949c936dc0aa8af6910b2d
Erick <erickungu82@gmail.com>
2023-10-08 14:17:37 +0300

    Update the means to add the sketch divs
    Instead of adding 256 divs to the sketch container the means was updated
    to add divs according to row and column

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const containerDiv = document.querySelector('.container')
+const sketchFace = document.querySelector('.sketch-face')
+const colorSelection = document.querySelector('.color-selection')
 
-for (let i = 1; i < 257; i++){
-    const gridDivs = document.createElement('div');
-    containerDiv.appendChild(gridDivs)
+for (let i = 1; i < 16; i++) {
+    const gridRow = document.createElement('div');
+    gridRow.className = 'grid-row';
+    sketchFace.appendChild(gridRow);
+}
+
+for (let j = 1; j < 16; j++) {
+    const gridColumn = document.createElement('div');
+    gridColumn.className = 'grid-column';
+    sketchFace.appendChild(gridColumn);
 }
