dc80f30be971f7feabb69bda07a64159f31ad5b0
Erick <erickungu82@gmail.com>
2023-10-08 17:17:26 +0300

    Update the makeGrid function to take a size argument instead of a fixed size

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const containerDiv = document.querySelector('.container')
 const sketchFace = document.querySelector('.sketch-face')
 const colorSelection = document.querySelector('.color-selection')
 
-function makeGrid() {
-    for (let i = 1; i <= 16; i++) {
-        const gridRow = document.createElement('div');
-        gridRow.className = 'grid-row';
-        sketchFace.appendChild(gridRow);
-    }
+function makeGrid(size) {
+    sketchFace.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    sketchFace.style.gridTemplateColumns = `repeat(${size}, 1fr)`
 
-    for (let j = 1; j <= 16; j++) {
-        const gridColumn = document.createElement('div');
-        gridColumn.className = 'grid-column';
-        sketchFace.appendChild(gridColumn);
+
+    let numDivs = size * size;
+
+    for (let i = 0; i < numDivs; i++) {
+        let cell = document.createElement('div');
+        cell.className = 'cell';
+        sketchFace.appendChild(cell);
     }
 }
 
-makeGrid()
+
+
+
+
+makeGrid(16)
