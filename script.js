4aaeaec77ac72ceb41d68a12082a3a2b33ea855b
erick <erickungu82@gmail.com>
2023-10-12 09:40:17 +0300

    Add funtionality allowing a user to input the number of grids needed

--- script.js
+++ script.js
@@ -2,8 +2,31 @@ const containerDiv = document.querySelector('.container')
 const sketchFace = document.querySelector('.sketch-face')
 const colorSelection = document.querySelector('.color-selection')
 const sizeSelector = document.querySelector('.grid-selection')
+const submitSize = document.querySelector('.submit-selection')
 
-function makeGrid(size) {
+// let size = parseInt(sizeSelector.value)
+// function makeGrid(size) {
+    
+//     sketchFace.style.gridTemplateRows = `repeat(${size}, 1fr)`
+//     sketchFace.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+
+
+//     let numDivs = size * size;
+
+//     for (let i = 0; i < numDivs; i++) {
+//         let cell = document.createElement('div');
+//         cell.className = 'cell';
+//         sketchFace.appendChild(cell);
+//     }
+// }
+
+// makeGrid(size)
+
+// console.log(typeof size)
+
+submitSize.addEventListener('click', function () {
+    let size = parseInt(sizeSelector.value)
+
     sketchFace.style.gridTemplateRows = `repeat(${size}, 1fr)`
     sketchFace.style.gridTemplateColumns = `repeat(${size}, 1fr)`
 
@@ -15,13 +38,8 @@ function makeGrid(size) {
         cell.className = 'cell';
         sketchFace.appendChild(cell);
     }
-}
-
-console.log(sizeSelector.value)
+})
 
 
-makeGrid(16)
 
-function getSize(){
-    
-}
+
