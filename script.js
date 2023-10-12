f3d2d7e22f5e318104ad317f0f72892d868bebcc
erick <erickungu82@gmail.com>
2023-10-12 10:32:56 +0300

    Add functionality to the reset button

--- script.js
+++ script.js
@@ -3,32 +3,13 @@ const sketchFace = document.querySelector('.sketch-face')
 const colorSelection = document.querySelector('.color-selection')
 const sizeSelector = document.querySelector('.grid-selection')
 const submitSize = document.querySelector('.submit-selection')
-
-// let size = parseInt(sizeSelector.value)
-// function makeGrid(size) {
-    
-//     sketchFace.style.gridTemplateRows = `repeat(${size}, 1fr)`
-//     sketchFace.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-
-
-//     let numDivs = size * size;
-
-//     for (let i = 0; i < numDivs; i++) {
-//         let cell = document.createElement('div');
-//         cell.className = 'cell';
-//         sketchFace.appendChild(cell);
-//     }
-// }
-
-// makeGrid(size)
-
-// console.log(typeof size)
+const resetButton = document.querySelector('.reset')
 
 submitSize.addEventListener('click', function () {
     let size = parseInt(sizeSelector.value)
 
     sketchFace.innerHTML = '';
-    
+
     sketchFace.style.gridTemplateRows = `repeat(${size}, 1fr)`
     sketchFace.style.gridTemplateColumns = `repeat(${size}, 1fr)`
 
@@ -43,6 +24,12 @@ submitSize.addEventListener('click', function () {
 
     console.log(numDivs)
 })
+
+resetButton.addEventListener('click' , function(){
+    sketchFace.innerHTML = ''
+})
+
+
 
 
 
