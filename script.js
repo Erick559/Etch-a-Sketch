f5d755df7f746d3d6b527e4bf420b3444fe7fcd5
Erick <erickungu82@gmail.com>
2023-10-13 19:55:32 +0300

    Add the hover state to each cell of the grid where their color changes once hovered on

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const colorSelection = document.querySelector('.color-selection')
 const sizeSelector = document.querySelector('.grid-selection')
 const submitSize = document.querySelector('.submit-selection')
 const resetButton = document.querySelector('.reset')
+const btnColor = document.querySelectorAll('[data-selection]')
+let rainbowColors = ['red', 'blue', 'purple', 'violet', 'orange']
+
 
 function makeGrid() {
     let size = parseInt(sizeSelector.value)
@@ -22,7 +25,23 @@ function makeGrid() {
         sketchFace.appendChild(cell);
     }
     let cells = document.querySelectorAll('.cell');
-    console.log(cells.length);
+
+    btnColor.forEach((button, index) => button.addEventListener(
+        'click', () => {
+            let colorSelected = button.getAttribute('data-selection');
+            for (let box of cells) {
+                if (colorSelected == 'rainbow') {
+                    box.addEventListener('mouseover', () => {
+                        box.style.backgroundColor = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+                    })
+                } else {
+                    box.addEventListener('mouseover', () => {
+                        box.style.backgroundColor = colorSelected
+                    })
+                }
+            }
+        }
+    ))
 }
 
 makeGrid()
